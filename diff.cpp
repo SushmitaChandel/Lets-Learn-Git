2456a9ecbc83ff27758f6fa7d207153c53aea379
Sushmita Chandel <sushmita.20.5@gmail.com>
2021-09-09 12:31:05 +0530

    modified diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
 {
 	int a = 5;
 	int b = 10;
-	cout<<a+b;
-	cout<<"Sum is displayed above";
+	cout<<a-b;
+	cout<<"difference is displayed above";
 	return 0;
 }
